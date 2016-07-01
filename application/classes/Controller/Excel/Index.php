9453282a511fd1fb14f1c30ced116a87161d1013
khaydarov <murod.haydarov@inbox.ru>
2016-07-01 15:14:53 +0300

    Creating WorkBook when new event added

---
+++ application/classes/Controller/Excel/Index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @team ProNWE team
+ * @author Khaydarov Murod
+ */
+
+class Controller_Excel_Index {
+
+    public function __construct()
+    {
+    }
+
+    
+
+}
--- application/classes/ORM/Score.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-class ORM_Score extends ORM {
-    protected $_table_name = 'Scores';
-    protected $_primary_key = '1';
-}
-
-?>
