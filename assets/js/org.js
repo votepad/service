77657a10bf74a244ffb888a198a731eef4871bf6
kolyasha <turov96@yandex.ru>
2016-07-31 17:54:29 +0300

    team page has done

--- assets/js/org.js
+++ assets/js/org.js
@@ -17,6 +17,19 @@ $(function () {
     }
   });
 
+  $(".newUser").on("click", function(){
+    $(".newUser").css("display","none");
+    $(".newUserForm").css("display","block");
+  });
+  $(".eventsToUser").select2({
+    language: "ru"
+  });
+  $("#canselnewUser").on("click", function(){
+    $(".newUser").css("display","block");
+    $(".newUserForm").css("display","none");
+    $('form[name=newUserForm]').trigger('reset');
+    $(".eventsToUser").select2("val", "");
+  });
 });
 
 
