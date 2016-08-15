316825d005f3ce60db9fe5d190c343a6ca110d66
kolyasha <turov96@yandex.ru>
2016-08-15 21:36:12 +0300

    improve about-event

--- assets/js/events/event-about.js
+++ assets/js/events/event-about.js
@@ -50,4 +50,42 @@ $().ready (function() {
 		$('#edit_event_about').css('display','block');
 		check();
 	});
+
+
+	$('#shortdesc_max_length').text(parseInt(170-$('#eventshortdesc').val().length));
+	$('#eventshortdesc').keyup(function(){
+		$('#shortdesc_max_length').text(parseInt(170-$('#eventshortdesc').val().length));
+	});
+	$('#event_main_info').validate({
+		errorClass: "error-input",
+		rules: {
+			eventname: "required",
+			eventsite: "required",
+			eventshortdesc: "required",
+			eventstart: "required",
+			eventend: "required",
+			eventstatus: "required",
+			eventcity: "required",
+			email: {
+				required: true,
+				email: true
+			},
+			confirmrools: "required"
+		},
+		messages: {
+			eventname: "Пожалуйста, введите название мероприятия.",
+			eventsite: "Пожалуйста, введите адрес мероприятия",
+			eventshortdesc: "Пожалуйста, расскажите кратко о своём мероприятии.",
+			eventstart: "Пожалуйста, введите дату начала мероприятия.",
+			eventend: "Пожалуйста, введите дату завершения мероприятия.",
+			eventstatus: "Пожалуйста, выберите статус мероприятия.",
+			eventcity: "Пожалуйста, выберите город, в котором пройдет мероприятие.",
+			email: {
+				required: "Пожалуйста, введите адрес электронной почты.",
+				email: "Пожалуйста, проверьте правильность ввода адреса электронной почты."
+			},
+			confirmrools: "Пожалуйста, прочитайте и согласитесь с правилами."
+		},
+	});
+
 });
