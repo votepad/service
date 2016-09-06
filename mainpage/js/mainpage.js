5f45c898eada01c5d64af172f7068de238220cbd
Gilevich <unvir@yandex.ru>
2016-09-06 16:17:01 +0300

    EventStrem версия пк

---
+++ mainpage/js/mainpage.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+	
+	$('.site_description').fadeIn(1250,function(){
+			$('#enter_button').fadeIn(1250);
+		});
+	
+	$(".news-item p").each(function(){	
+			
+		var max_short_news_length = 200;
+		var n = $(this).html().length;
+		
+		if ( n > max_short_news_length ) // отсекаем длинный текст новости
+		{
+			var content = $(this).html().substr(0,max_short_news_length);
+			content+='...';
+			$(this).html(content);
+		}
+	});
+	
+});
+
+$('#sort_type, #sort_city').change(function(){
+	$('#sort').submit();
+});
