09301198638e060db096d7dfcc06ac3a72f9db1d
khaydarov <murod.haydarov@inbox.ru>
2016-09-23 00:54:28 +0300

    adding full description

--- application/classes/Model/Events.php
+++ application/classes/Model/Events.php
@@ -1,13 +1,26 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 /**
- * Created by PhpStorm.
  * User: Murod's Macbook Pro
- * Date: 16.03.2016
- * Time: 22:29
+ * @author Pronwe Team
+ * @copyright Khaydarov Murod
  */
 
 class Model_Events extends Model {
 
+
+    /**
+     * Creates new Event
+     * @uses Kohana ORM
+     * @param $id_organization
+     * @param $name
+     * @param $page
+     * @param $description
+     * @param $start_time
+     * @param $end_time
+     * @param $status
+     * @param $city
+     * @return Events identificator
+     */
     public static function new_event($id_organization, $name, $page, $description,
                         $start_time, $end_time, $status, $city)
     {
@@ -28,6 +41,44 @@ class Model_Events extends Model {
         return $event->id;
     }
 
+    /**
+     * Gets event information
+     * @param $id
+     * @return ORM_Events object
+     * @throws Kohana_Exception
+     */
+    public static function get($id)
+    {
+
+        $event = new ORM_Events();
+
+        $event
+            ->where('id', '=', $id)
+            ->find();
+
+        if ($event->loaded()) {
+            return $event;
+        }
+    }
+
+    /**
+     * Updates field
+     * @param $id
+     * @param $field
+     * @param $value
+     */
+    public static function updateField($id, $field, $value)
+    {
+        $target = self::get($id);
+
+        $target->$field = $value;
+        $target->save();
+    }
+
+    /**
+     * @param $id
+     * @return mixed
+     */
     public static function getOrganizationEvents($id)
     {
         $select = DB::select()->from('Events')
@@ -38,6 +89,10 @@ class Model_Events extends Model {
         return $select;
     }
 
+    /**
+     * @param $name
+     * @return mixed
+     */
     public static function getEventByName($name)
     {
         $event = DB::select()->from('Events')
