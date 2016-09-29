d3e135ea828182bd584e162ac709299c329ca88b
Gilevich <unvir@yandex.ru>
2016-09-29 16:26:20 +0300

    70% organisation completed

--- application/views/events/all.php
+++ application/views/events/all.php
@@ -23,120 +23,111 @@
       </div>
   </div>
 
+	<!-- SEARCHING  -->
+	<div class="panel panel-default searching">
+		<div class="panel-body">
+			<ul class="no-li">
+				<li class="inline">
+					Поиск мероприятия
+				</li>
+				<li class="inline search-block" id="search-block" style="width:calc(100%)">
+					<input name="event_name" type="text" class="" style="width:70%;">
+					
+					<i class="fa fa-users" aria-hidden="true"></i>
+					<select class="nodisp" name="event_sort">
+						<option></option>
+						<option>Название мероприятия</option>
+						<option>Дата начала мероприятия</option>
+					</select>
 
+					<i class="fa fa-users" aria-hidden="true"></i>
+					<select class="nodisp" name="event_type">
+						<option></option>
+						<option>Черновик</option>
+						<option>Виден всем</option>
+						<option>Виден команде</option>
+					</select>
+				</li>
+			</ul>
+		</div>
+	</div>
+<div id="count_events" class="text-center"></div>
+	<!-- LIST OF EVENTS -->
+	<ul id="events_list" class="text-center">
+		<? for($i = 0; $i < count($events); $i++) : ?>
+		<li class="event-group">
+			<div class="event-wrapper">
+				<div class="event-shot">
+					<div class="event-image" style="background: url(<?=$assets; ?>img/bg2.jpg) no-repeat;"></div>
+					<a class="event-link" href="<?=URL::site('events/' . $events[$i]['name']); ?>">
+						<div class="event-preview">
+							<h2 class="event_name_search"><?=$events[$i]['name']; ?></h2>
+							<p><?=$events[$i]['short_description']; ?></p>
+							<span class="event_time_search"><?=$events[$i]['start_time']; ?></span>
+							<small class="event_type_search">черновик</small>
+						</div>
+					</a>
+					<div class="event-result" style="display: block">
+						<a href="#/tnl/rating" data-toggle="tooltip" data-placement="top" title="Рейтинг">
+							<i class="fa fa-users" aria-hidden="true"></i>
+						</a>
+					</div>
+				</div>
+				<div class="event-footer">
+					<ul class="event-footer-left">
+						<li class="li">
+							<div class="">
+								<button data-toggle="dropdown" class="md-btn item">
+									<i class="fa fa-share-alt" aria-hidden="true"></i>
+								</button>
+								<ul role="menu" class="dropdown-menu">
+									<li class="vk">
+										<a href="#">
+											<i class="fa fa-vk" aria-hidden="true"></i>
+											<span>Вконтакте</span>
+										</a>
+									</li>
+									<li>
+										<a href="#" class="facebook">
+											<i class="fa fa-facebook" aria-hidden="true"></i>
+											<span>Facebook</span>
+										</a>
+									</li>
+									<li>
+										<a href="#" class="twitter">
+											<i class="fa fa-twitter" aria-hidden="true"></i>
+											<span>Twitter</span>
+										</a>
+									</li>
+								</ul>
+							</div>
+						</li>
+						<li class="li">
+							<button type="button" class="md-btn item" data-toggle="tooltip" data-placement="bottom" title="Связаться с организатором" onclick="open_feedback_form(email)">
+								<i class="fa fa-envelope" aria-hidden="true"></i>
+							</button>
+						</li>
+					</ul>
+					<ul class="event-footer-right">
+						<li class="fav">
+							<button type="button" class="md-btn">
+								<i class="fa fa-heart" aria-hidden="true"></i>
+								<span>165</span>
+							</button>
+						</li>
+						<li class="views">
+							<div class="div-views md-btn" data-toggle="tooltip" data-placement="bottom" title="Просмотров">
+								<i class="fa fa-eye" aria-hidden="true"></i>
+								<span>215</span>
+							</div>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</li>
+		<? endfor; ?>
+	</ul>
 </div>
-    <div class="left-column">
-        <div class="panel block">
-            <div class="panel-body">
-                <div class="row">
-    
-                    <div class="form-group search-block">
-                        <div class="col-md-7 col-xs-12">
-                            <label class="control-label">Поиск мероприятия</label>
-                            <input name="event_name" type="text" class="form-control input-sm">
-                        </div>
-
-                        <div class="col-md-3 col-xs-6">
-                            <label class="control-label">Сортировать по</label>
-                            <select class="form-control input-sm" name="event_sort">
-                                <option></option>
-                                <option>Название мероприятия</option>
-                                <option>Дата начала мероприятия</option>
-                            </select>
-                        </div>
-
-                        <div class="col-md-2 col-xs-5 col-xs-offset-1 col-md-offset-0">
-                            <label class="control-label">Тип</label>
-                            <select class="form-control input-sm" name="event_type">
-                                <option></option>
-                                <option>Черновик</option>
-                                <option>Виден всем</option>
-                                <option>Виден команде</option>
-                            </select>
-                        </div>
-                    </div>
-
-                    <!-- LIST OF EVENTS -->
-                    <ul id="events_list" class="text-center">
-                        <? for($i = 0; $i < count($events); $i++) : ?>
-                        <li class="event-group">
-                            <div class="event-wrapper">
-                                <div class="event-shot">
-                                    <div class="event-image" style="background: url(<?=$assets; ?>img/bg2.jpg) no-repeat;"></div>
-                                    <a class="event-link" href="<?=URL::site('events/' . $events[$i]['name']); ?>">
-                                        <div class="event-preview">
-                                            <h2 class="event_name_search"><?=$events[$i]['name']; ?></h2>
-                                            <p><?=$events[$i]['short_description']; ?></p>
-                                            <span class="event_time_search"><?=$events[$i]['start_time']; ?></span>
-                                            <small class="event_type_search">черновик</small>
-                                        </div>
-                                    </a>
-                                    <div class="event-result" style="display: block">
-                                        <a href="#/tnl/rating" data-toggle="tooltip" data-placement="top" title="Рейтинг">
-                                            <i class="fa fa-users" aria-hidden="true"></i>
-                                        </a>
-                                    </div>
-                                </div>
-                                <div class="event-footer">
-                                    <ul class="event-footer-left">
-                                        <li class="li">
-                                            <div class="">
-                                                <button data-toggle="dropdown" class="md-btn item">
-                                                    <i class="fa fa-share-alt" aria-hidden="true"></i>
-                                                </button>
-                                                <ul role="menu" class="dropdown-menu">
-                                                    <li class="vk">
-                                                        <a href="#">
-                                                            <i class="fa fa-vk" aria-hidden="true"></i>
-                                                            <span>Вконтакте</span>
-                                                        </a>
-                                                    </li>
-                                                    <li>
-                                                        <a href="#" class="facebook">
-                                                            <i class="fa fa-facebook" aria-hidden="true"></i>
-                                                            <span>Facebook</span>
-                                                        </a>
-                                                    </li>
-                                                    <li>
-                                                        <a href="#" class="twitter">
-                                                            <i class="fa fa-twitter" aria-hidden="true"></i>
-                                                            <span>Twitter</span>
-                                                        </a>
-                                                    </li>
-                                                </ul>
-                                            </div>
-                                        </li>
-                                        <li class="li">
-                                            <button type="button" class="md-btn item" data-toggle="tooltip" data-placement="bottom" title="Связаться с организатором" onclick="open_feedback_form(email)">
-                                                <i class="fa fa-envelope" aria-hidden="true"></i>
-                                            </button>
-                                        </li>
-                                    </ul>
-                                    <ul class="event-footer-right">
-                                        <li class="fav">
-                                            <button type="button" class="md-btn">
-                                                <i class="fa fa-heart" aria-hidden="true"></i>
-                                                <span>165</span>
-                                            </button>
-                                        </li>
-                                        <li class="views">
-                                            <div class="div-views md-btn" data-toggle="tooltip" data-placement="bottom" title="Просмотров">
-                                                <i class="fa fa-eye" aria-hidden="true"></i>
-                                                <span>215</span>
-                                            </div>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </li>
-                        <? endfor; ?>
-                    </ul>
-                    <div id="count_events" class="text-center"></div>
-                </div>
-            </div>
-        </div>
-    </div>
-
     <!-- RIGHT COLUMN --
     <div class="right-column">
         <div class="panel panel-default block">
--- application/views/organizations/blocks/navigation.php
+++ application/views/organizations/blocks/navigation.php
@@ -1,10 +1,29 @@
 <div class="org-nav">
-  <a href="<?=URL::site('organization/' . $id); ?>" class="md-btn">
+  <a href="<?=URL::site('organization/' . $id); ?>" class="menu-btn">
       Мероприятия
-      <div class="active-tab"></div>
   </a>
-  <a href="<?=URL::site('organization/' . $id . '/settings/main'); ?>" class="md-btn">
+  <a href="<?=URL::site('organization/' . $id . '/settings/main'); ?>" class="menu-btn">
       Настройки
-      <div class="active-tab"></div>
   </a>
+  
+  <div class="dropdown menu-btn nodisp">
+	<a href="#" id="dLabel" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+		Быстрый старт
+		<span class="caret"></span>
+	</a>
+	<ul class="dropdown-menu" aria-labelledby="dLabel">
+		<li>
+			<a href="#" class="">
+				<i class="fa fa-plus" aria-hidden="true"></i>
+				Создать мероприятие
+			</a>
+		</li>
+		<li>
+			<a href="#" class="">
+				<i class="fa fa-paper-plane" aria-hidden="true"></i>
+				Пригласить организатора
+			</a>
+		</li>
+	</ul>
+	</div>
 </div>
