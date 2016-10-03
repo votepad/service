312fe8d96fc0b9b57f4b2a4447097d0073a9175a
Gilevich <unvir@yandex.ru>
2016-10-03 16:01:29 +0300

    all.php Небольшие правки по дизайну, остался footer

--- application/views/events/all.php
+++ application/views/events/all.php
@@ -1,7 +1,7 @@
 <div class="columns-area">
   <div class="panel panel-default quick-start">
       <div class="panel-body">
-        <ul class="no-li">
+        <ul class="no-li clearfix">
           <li class="inline">
             <div class="">
               Быстрый старт
@@ -82,101 +82,20 @@
           <i class="fa fa-calendar" aria-hidden="true"></i>
           <span class="event_time_search"><?=$events[$i]['start_time']; ?></span>
         </p>
-        <p>
+        <p class="event-status bgcolor_red">
           <i class="fa fa-eye" aria-hidden="true"></i>
           <span class="event_type_search">черновик</span>
         </p>
       </div>
-      <div class="event_card-action">
-        <a href="<?=URL::site('events/' . $events[$i]['name']); ?>">Страница</a>
-        <a href="<?=URL::site('events/' . $events[$i]['name']); ?>/results">Результаты</a>
+      <div class="event_card-action clearfix">
+        <a href="<?=URL::site('events/' . $events[$i]['name']); ?>" class="underlinehover pull-left">Страница</a>
+        <a href="<?=URL::site('events/' . $events[$i]['name']); ?>/results" class="underlinehover pull-right">Результаты</a>
       </div>
     </div>
   </li>
   <? endfor; ?>
 </ul>
 
-
-
-
-
-<div id="count_events" class="text-center"></div>
-	<!-- LIST OF EVENTS -->
-	<ul id="events_list" class="text-center">
-		<? for($i = 0; $i < count($events); $i++) : ?>
-		<li class="event-group">
-			<div class="event-wrapper">
-				<div class="event-shot">
-					<div class="event-image" style="background: url(<?=$assets; ?>img/bg2.jpg) no-repeat;"></div>
-					<a class="event-link" href="<?=URL::site('events/' . $events[$i]['name']); ?>">
-						<div class="event-preview">
-							<h2 class="event_name_search"><?=$events[$i]['name']; ?></h2>
-							<p><?=$events[$i]['short_description']; ?></p>
-							<span class="event_time_search"><?=$events[$i]['start_time']; ?></span>
-							<small class="event_type_search">черновик</small>
-						</div>
-					</a>
-					<div class="event-result" style="display: block">
-						<a href="#/tnl/rating" data-toggle="tooltip" data-placement="top" title="Рейтинг">
-							<i class="fa fa-users" aria-hidden="true"></i>
-						</a>
-					</div>
-				</div>
-				<div class="event-footer">
-					<ul class="event-footer-left">
-						<li class="li">
-							<div class="">
-								<button data-toggle="dropdown" class="md-btn item">
-									<i class="fa fa-share-alt" aria-hidden="true"></i>
-								</button>
-								<ul role="menu" class="dropdown-menu">
-									<li class="vk">
-										<a href="#">
-											<i class="fa fa-vk" aria-hidden="true"></i>
-											<span>Вконтакте</span>
-										</a>
-									</li>
-									<li>
-										<a href="#" class="facebook">
-											<i class="fa fa-facebook" aria-hidden="true"></i>
-											<span>Facebook</span>
-										</a>
-									</li>
-									<li>
-										<a href="#" class="twitter">
-											<i class="fa fa-twitter" aria-hidden="true"></i>
-											<span>Twitter</span>
-										</a>
-									</li>
-								</ul>
-							</div>
-						</li>
-						<li class="li">
-							<button type="button" class="md-btn item" data-toggle="tooltip" data-placement="bottom" title="Связаться с организатором" onclick="open_feedback_form(email)">
-								<i class="fa fa-envelope" aria-hidden="true"></i>
-							</button>
-						</li>
-					</ul>
-					<ul class="event-footer-right">
-						<li class="fav">
-							<button type="button" class="md-btn">
-								<i class="fa fa-heart" aria-hidden="true"></i>
-								<span>165</span>
-							</button>
-						</li>
-						<li class="views">
-							<div class="div-views md-btn" data-toggle="tooltip" data-placement="bottom" title="Просмотров">
-								<i class="fa fa-eye" aria-hidden="true"></i>
-								<span>215</span>
-							</div>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</li>
-		<? endfor; ?>
-	</ul>
-</div>
 <!-- =============== PAGE SCRIPTS ===============-->
 <script type="text/javascript" src="../../../assets/vendor/bootstrap/dist/js/bootstrap-dropdown.js"></script>
 <script type="text/javascript" src="<?=$assets; ?>js/organizations/org-all.js"></script>
