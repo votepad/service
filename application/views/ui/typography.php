b0f8370f6b0837cc02c4a3339ed86e3731c230b1
Nikolay <turov96@yandex.ru>
2016-11-10 13:43:10 +0300

    Refactoring Typography (compleate 100%)

--- application/views/ui/typography.php
+++ application/views/ui/typography.php
@@ -1,126 +1,131 @@
+<div class="row">
+    <h3 class="page-header">Headers</h3>
 
-  <div class="row">
-    <div class="col-lg-4 col-md-6 col-xs-12">
-      <div class="block block-default">
-        <div class="block-heading">
-          <h4>Headers</h4>
-        </div>
-        <div class="block-body">
-          <h1>Header 1
-             <small>Sub-header</small>
-          </h1>
-		  <blockquote>
-             <?php
-				echo htmlspecialchars("<h1>Header 1 <small>Sub-header</small></h1>");
-			 ?>
-          </blockquote>
-          <h2>Header 2
-             <small>Sub-header</small>
-          </h2>
-		  <blockquote>
-             <?php
-				echo htmlspecialchars("<h2>Header 2<small>Sub-heading</small></h2>");
-			 ?>
-          </blockquote>
-          <h3>Header 3
-             <small>Sub-header</small>
-          </h3>
-		  <blockquote>
-             <?php
-				echo htmlspecialchars("<h3>Header 3<small>Sub-heading</small></h3>");
-			 ?>
-          </blockquote>
-          <h4>Header 4
-             <small>Sub-header</small>
-          </h4>
-		  <blockquote>
-             <?php
-				echo htmlspecialchars("<h4>Header 4<small>Sub-heading</small></h4>");
-			 ?>
-          </blockquote>
-        </div>
-      </div>
+    <div class="row">
+        <h1>Header 1
+            <small>Sub-header</small>
+        </h1>
+        <blockquote>
+<xmp><h1>Header 1
+    <small>Sub-header</small>
+</h1></xmp>
+        </blockquote>
+
+        <h2>Header 2
+            <small>Sub-header</small>
+        </h2>
+        <blockquote>
+<xmp><h2>Header 2
+    <small>Sub-header</small>
+</h2></xmp>
+        </blockquote>
+
+        <h3>Header 3
+            <small>Sub-header</small>
+        </h3>
+        <blockquote>
+<xmp><h3>Header 3
+    <small>Sub-header</small>
+</h3></xmp>
+        </blockquote>
+
+        <h4>Header 4
+            <small>Sub-header</small>
+        </h4>
+        <blockquote>
+<xmp><h4>Header 4
+    <small>Sub-header</small>
+</h4></xmp>
+        </blockquote>
     </div>
-    <div class="col-lg-4 col-md-6 col-xs-12">
-      <div class="block block-default">
-        <div class="block-heading">
-          <h4>Paragraphs</h4>
-        </div>
-        <div class="block-body">
-          <p>This is an example of standard paragraph text. This is an example of <a href="#" class="underlinehover">link anchor text</a> within body copy.</p>
-		  <blockquote>
-             <?php
-				echo htmlspecialchars("<a href=\"#\" class=\"underlinehover\">link anchor text</a> ");
-			 ?>
-          </blockquote>
-          <p>
-             <small>This is an example of small, fine print text.</small>
-          </p>
-		  <blockquote>
-             <?php
-				echo htmlspecialchars("<small>This is an example of small, fine print text.</small>");
-			 ?>
-          </blockquote>
-          <p>
-             <strong>This is an example of strong, bold text.</strong>
-          </p>
-		  <blockquote>
-             <?php
-				echo htmlspecialchars("<strong>This is an example of strong, bold text.</strong>");
-			 ?>
-          </blockquote>
-          <p>
-             <em>This is an example of emphasized, italic text.</em>
-          </p>
-		  <blockquote>
-             <?php
-				echo htmlspecialchars("<em>This is an example of emphasized, italic text.</em>");
-			 ?>
-          </blockquote>
-          <br>
-          <h4>Alignment Helpers</h4>
-          <p class="text-left">Left aligned text.</p>
-          <p class="text-center">Center aligned text.</p>
-          <p class="text-right">Right aligned text.</p>
-        </div>
-      </div>
+
+
+    <h3 class="page-header">Paragraphs</h3>
+
+    <div class="row">
+        <p>This is an example of standard paragraph text. This is an example of <a href="#" class="underlinehover">link anchor text</a> within body copy.</p>
+        <blockquote>
+            <xmp><p> </p></xmp>
+            <br>
+            <xmp><a href="#" class="underlinehover"></a></xmp>
+        </blockquote>
+
+        <p>
+            <small>This is an example of small, fine print text.</small>
+        </p>
+        <blockquote>
+            <xmp><small> </small></xmp>
+        </blockquote>
+
+        <p>
+         <strong>This is an example of strong, bold text.</strong>
+        </p>
+        <blockquote>
+            <xmp><strong> </strong></xmp>
+            <br>
+            <xmp><b> </b></xmp>
+        </blockquote>
+
+        <p>
+         <em>This is an example of emphasized, italic text.</em>
+        </p>
+        <blockquote>
+            <xmp><em> </em></xmp>
+        </blockquote>
     </div>
-    <div class="col-lg-4 col-md-6 col-xs-12">
-      <div class="block block-default">
-        <div class="block-heading">
-          <h4>Blockquotes</h4>
-        </div>
-        <div class="block-body">
-          <h4>Default Blockquote</h4>
-          <blockquote>
+
+
+    <h3 class="page-header">Alignment Helpers</h3>
+
+    <div class="row">
+        <p class="text-left">Left aligned text.</p>
+        <blockquote>
+            <xmp><p class="text-left"></p></xmp>
+        </blockquote>
+        <p class="text-center">Center aligned text.</p>
+        <blockquote>
+            <xmp><p class="text-center"></p></xmp>
+        </blockquote>
+        <p class="text-right">Right aligned text.</p>
+        <blockquote>
+            <xmp><p class="text-right"></p></xmp>
+        </blockquote>
+    </div>
+
+
+    <h3 class="page-header">Blockquotes</h3>
+
+    <div class="row row-col">
+        <div class="col-xs-12 col-md-4">
+            <h4>Default Blockquote</h4>
+            <blockquote>
              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-          </blockquote>
-          <h4>Blockquote with Citation</h4>
-          <blockquote>
+            </blockquote>
+
+<blockquote><xmp><blockquote>
+    <p> </p>
+</blockquote></xmp></blockquote>
+        </div>
+        <div class="col-xs-12 col-md-4">
+            <h4>Blockquote with Citation</h4>
+            <blockquote>
              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
              <small>Someone famous in Source Title</small>
-          </blockquote>
-          <h4>Right Aligned Blockquote</h4>
-          <blockquote class="pull-right">
+            </blockquote>
+<blockquote><xmp><blockquote>
+    <p> </p>
+    <small> </small>
+</blockquote></xmp></blockquote>
+        </div>
+        <div class="col-xs-12 col-md-4">
+            <h4>Right Aligned Blockquote</h4>
+            <blockquote class="pull-right">
              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-          </blockquote>
+            </blockquote>
+<blockquote><xmp><blockquote class="pull-right">
+    <p> </p>
+    <small> </small>
+</blockquote></xmp></blockquote>
         </div>
-      </div>
     </div>
-    <div class="col-lg-5 col-md-6 col-xs-12">
-      <div class="block block-default">
-        <div class="block-heading">
-          <h4>Links</h4>
-        </div>
-        <div class="block-body">
-          <a href="#" class="">default</a>
-          <br><br>
-          <a href="#" class="underlinehover">underlinehover (hover and focus)</a>
-          <br><br>
-          <a href="#" class="link_uppercase">link_uppercase</a>
-          <br><br>
-          <a href="#" class="link_lowercase">link_lowercase</a>
-        </div>
-      </div>
-    </div>
-  </div>
+</div>
