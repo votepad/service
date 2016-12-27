82b1eb85d9f275c0a487ab9b05a24818fff7c55f
Nikolay <turov96@yandex.ru>
2016-12-28 01:16:03 +0300

    сделан редизайн header'а + адаптив

--- application/views/events/main.php
+++ application/views/events/main.php
@@ -19,53 +19,50 @@
 <body>
 <header class="header">
     <div class="header_wrapper">
-        <div class="header_menu-collapse-btn">
-            <button id="open_leftnav" class="header_button">
+
+        <div class="header_menu-btn-icon left">
+            <button id="open_menu" class="header_button">
                 <i class="fa fa-bars header_icon" aria-hidden="true"></i>
             </button>
         </div>
 
-        <div class="header_logo">NWE</div>
+        <div class="header_text header_text-logo">VotePad</div>
 
         <div class="header_menu">
             <?=$top; ?>
         </div>
 
-        <div class="header_dropdown dropdown">
-
+        <div class="header_menu-dropdown dropdown">
             <a id="open_usermenu" class="header_button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <span class="header_text">Николай</span>
+                <span class="header_text">Nikolay</span>
                 <i class="fa fa-caret-down header_icon" aria-hidden="true"></i>
             </a>
-
-            <ul class="dropdown-menu pull-right" aria-labelledby="open_usermenu">
-                <li class="nav_item">
-                    <a href="#" class="nav_link">
-                        <i class="fa fa-cubes nav_icon header_icon" aria-hidden="true"></i>
-                        Мои мероприятия
-                    </a>
-                </li>
-                <li class="nav_item">
-                    <a href="#" class="nav_link">
-                        <i class="fa fa-user nav_icon header_icon" aria-hidden="true"></i>
-                        Профиль
-                    </a>
-                </li>
-                <li role="separator" class="divider"></li>
-                <li class="nav_item">
-                    <a href="#" class="nav_link">
-                        <i class="fa fa-sign-out nav_icon header_icon" aria-hidden="true"></i>
-                        Выйти
-                    </a>
-                </li>
-           </ul>
+            <div class="dropdown-menu pull-right" aria-labelledby="open_usermenu">
+                <a href="#" class="header_button dropdown-button">
+                    <i class="fa fa-cubes nav_icon dropdown-icon header_icon" aria-hidden="true"></i>
+                    <span class="dropdown-text">Мои мероприятия</span>
+                </a>
+                <a href="#" class="header_button dropdown-button">
+                    <i class="fa fa-user dropdown-icon header_icon" aria-hidden="true"></i>
+                    <span class="dropdown-text">Профиль</span>
+                </a>
+                <div role="separator" class="divider"></div>
+                <a href="#" class="header_button dropdown-button">
+                    <i class="fa fa-sign-out dropdown-icon header_icon" aria-hidden="true"></i>
+                    <span class="dropdown-text">Выйти</span>
+                </a>
+            </div>
         </div>
-    </div>
-    </header>
 
-    <nav class="navleft">
-        <?=$left ?>
-  	</nav>
+        <div class="header_menu-btn-icon right">
+            <button id="open_menu-page" class="header_button">
+                <i class="fa fa-ellipsis-v header_icon" aria-hidden="true"></i>
+            </button>
+        </div>
+
+    </div>
+</header>
+
 
     <section>
         <?=$main_section; ?>
--- assets/js/app_v1.js
+++ assets/js/app_v1.js
@@ -4,14 +4,21 @@
 
 $(document).ready(function(){
 
+    var start_on_small = false;
+    if ($('body').width() + 17 < 992){
+        start_on_small = true;
+    }
+
+
+
     /*
      *  Get Page Link and Add Class to Link
     */
     var address = window.location.pathname.split('/');
     address =   '/' + address[1] + '/' + address[2] + '/' + address[3];
-    $('.header_menu .nav_link').each(function(){
-        var temp = $(this).attr('href').split('/');// ;
-        temp = new RegExp('/' + temp[1] + '/' + temp[2] + '/' + temp[3]);
+    $('.header_menu .header_button').each(function(){
+        var temp = $(this).attr('href').split('/');
+        temp = new RegExp(temp[1] + '/' + temp[2] + '/' + temp[3]);
         if ( temp.test(address) ) {
             $(this).addClass('active');
         } else {
@@ -21,83 +28,49 @@ $(document).ready(function(){
 
 
 
-    /*
-     *  Header Menu And Left Navigation
-     *
-     *  @var header_menu_nav_items  -  array of header_menu elements
-     *  @var leftnav_elements_curent   -   array of navleft elements
-     *  @var temp
-    */
-
-     var
-        header_menu_nav_items = [],
-        leftnav_elements_curent = $('.navleft_wrapper').html(),
-        temp;
-
-
-     /*
-      *  Create header_menu_nav_items array
-     */
-
-     $('.header_menu .nav .nav_item').each(function(i){
-
-         temp = {
-             html: $(this).html(),
-             width: $(this).width()
-         }
-
-         header_menu_nav_items.push(temp);
-
-     });
-
-
      /*
       *  Show header_menu block
      */
 
-     $('.header_menu .nav').animateCss('fadeIn');
-     $('.header_menu .nav').css('opacity','1');
+     $('.header_menu').animateCss('fadeIn');
+     $('.header_menu').css('opacity','1');
 
 
      /*
-      *  Call 'header_menu_leftnav_fun' for creating header_menu and navleft elements
+      *  Call 'header_menu_fun' for creating header_menu and navleft elements
      */
-
-     header_menu_leftnav_fun();
+     var header_menu_nav_items = create_header_menu_elements();
+     header_menu_fun();
 
 
 
      /*
       *  Window Resize Function
       *
-      *  Call 'header_menu_leftnav_fun' for updating elements in header_menu and navleft
+      *  Call 'header_menu_fun' for updating elements in header_menu and navleft
      */
 
      $(window).resize(function(){
 
-         header_menu_leftnav_fun();
+         header_menu_fun();
 
      });
 
 
 
      /*
-      *
+      *  Open header_menu on tablet and mobile versions
      */
 
-     $('#open_leftnav').click(function(){
-
-        $('body').addClass('hidden').append('<div class="navleft-open_back"></div>');
-        $('.navleft').addClass('open');
-
+     $('#open_menu').click(function(){
+         $('body').addClass('hidden').append('<div class="backdrop"></div>');
+         $('.header_menu').addClass('open');
      });
 
-     $('body').on('click', '.navleft-open_back', function(){
-
+     $('body').on('click', '.backdrop', function(){
          $('body').removeClass('hidden')
-         $('.navleft').removeClass('open');
+         $('.header_menu').removeClass('open');
          $(this).remove();
-
      });
 
 
@@ -139,7 +112,7 @@ $(document).ready(function(){
 
      /*
       *  Functions
-      *  - header_menu_leftnav_fun()     -  creating and updating header_menu and navleft elements
+      *  - header_menu_fun()     -  creating and updating header_menu and navleft elements
       *
      */
 
@@ -152,22 +125,51 @@ $(document).ready(function(){
       *   @var header_menu_nav_items
      */
 
-     function header_menu_leftnav_fun() {
+     function header_menu_fun() {
+
+         if ( $('body').width() + 17 > 992 && start_on_small == true){
+             header_menu_nav_items = create_header_menu_elements();
+             start_on_small = false;
+             var count = 0;
+             for (var i = 0; i < header_menu_nav_items.length; i++) {
+                 if ( header_menu_nav_items[i].class == "header_global header_button dropdown-button") {
+                     count += 1;
+                 }
+             }
+             header_menu_nav_items.splice(0,count);
+         }
 
          /*
           *  Vars
          */
          var
-            header_menu_width = $('.header_menu .nav').width(), // curent width of header_menu
-            header_menu_nav_width = 65,  // width of header_menu_elements,  65 - min width for last element
+            header_menu_width = $('.header_menu').width(), // curent width of header_menu
+            header_menu_nav_width = 80,  // width of header_menu_elements,  65 - min width for last element
             header_menu_elements = '',  // elements which shows in header_menu
             header_menu_dropdown = '',  // header_dropdown block
             header_menu_dropdown_elements = '', // elements which shows in header_dropdown block
             header_menu_dropdown_pull_right,  // for style - right: __px
+            seperator = '',
             temp = true;  // temp - boolean for seperate elements from header_menu_nav_items to header_menu_elements and header_menu_dropdown_elements
 
 
-         $('.header_menu .nav').empty();
+         $('.header_menu').empty();
+
+         /*
+          * Add quick navigation on mobile vetsion
+         */
+         if ( $('body').width() + 17 < 681 ){
+             var
+                dropdown_global = '',
+                temp;
+
+             $('.header_menu-dropdown .dropdown-menu .header_button').each(function(){
+                 dropdown_global = dropdown_global +'<a href="' + $(this)[0]['href'] + '" class="header_global ' + $(this)[0]['className'] + '">' + $(this).html() + '</a>';
+             });
+
+             $('.header_menu').append('<p class="header_text header_text-title">Быстрая навигация</p><div class="divider"></div>' + dropdown_global);
+
+         }
 
 
          /*
@@ -176,81 +178,90 @@ $(document).ready(function(){
 
          for (var i = 0; i < header_menu_nav_items.length; i++) {
 
-             if ( header_menu_nav_width + header_menu_nav_items[i].width < header_menu_width && temp) {
-
+             if ( temp && (header_menu_nav_width + header_menu_nav_items[i].width) < header_menu_width) {
                  header_menu_nav_width = header_menu_nav_width + header_menu_nav_items[i].width;
-                 header_menu_elements = header_menu_elements + "<li class='nav_item'>" + header_menu_nav_items[i].html + "</li>";
-
+                 header_menu_elements = header_menu_elements + '<a href="' + header_menu_nav_items[i].href + '" class="' + header_menu_nav_items[i].class + '">' + header_menu_nav_items[i].html + '</a>';
              }
 
              else{
-
                  temp = false;
-                 header_menu_dropdown_elements = header_menu_dropdown_elements + "<li class='nav_item'>" + header_menu_nav_items[i].html + "</li>";
-
+                 var temp_span = $.trim(header_menu_nav_items[i].html);
+                 header_menu_dropdown_elements = header_menu_dropdown_elements + '<a href="' + header_menu_nav_items[i].href + '" class="' + header_menu_nav_items[i].class + ' dropdown-button">' + temp_span.substr(0,24) + ' dropdown-text' + temp_span.substr(24,temp_span.length) + '</a>';
              }
 
          }
 
+         if ($('body').width() + 17 > 992){
+             /*
+              *  Add pull-right width for header_menu_dropdown_pull_right
+              *
+              *  @var header_menu_width
+              *  @var header_menu_nav_width
+             */
 
-         /*
-          *  Add pull-right width for header_menu_dropdown_pull_right
-          *
-          *  @var header_menu_width
-          *  @var header_menu_nav_width
-         */
-
-         header_menu_dropdown_pull_right = header_menu_width - header_menu_nav_width;
-
-
-         /*
-          *  Checking header_menu_dropdown_elements for empty
-          *
-          *  @var header_menu_dropdown_elements
-         */
-
-         if (header_menu_dropdown_elements != '') {
-
-             header_menu_dropdown = '<div class="nav_item dropdown"><a id="header_menu_dropdown" class="nav_link" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><span class="header_text">Ещё</span><i class="fa fa-caret-down header_icon" aria-hidden="true"></i></a><ul class="dropdown-menu pull-right" style="right:' + header_menu_dropdown_pull_right + 'px" aria-labelledby="header_menu_dropdown">' + header_menu_dropdown_elements + '</ul></div>';
-
-         }
+             header_menu_dropdown_pull_right = header_menu_width - header_menu_nav_width;
 
 
-         /*
-          *  Append header_menu_elements and header_menu_dropdown to header_menu block
-          *
-          *  @var header_menu_elements
-          *  @var header_menu_dropdown
-         */
+             /*
+              *  Checking header_menu_dropdown_elements for empty
+              *
+              *  @var header_menu_dropdown_elements
+             */
 
-         $('.header_menu .nav').append(header_menu_elements + header_menu_dropdown);
+             if (header_menu_dropdown_elements != '') {
 
+                 header_menu_dropdown = '<div class="dropdown"><a id="header_menu_dropdown" class="header_button dropdown-button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><span class="header_text">Ещё</span><i class="fa fa-caret-down header_icon" aria-hidden="true"></i></a><div class="dropdown-menu pull-right" style="right:' + header_menu_dropdown_pull_right + 'px" aria-labelledby="header_menu_dropdown">' + header_menu_dropdown_elements + '</div></div>';
 
+             }
 
-         /*
-          *   Create NavLeft Element
-          *
-          *   @var  header_menu_elements
-          *   @var  header_menu_dropdown_elements
-          *   @var  leftnav_elements_curent
-         */
+             $('.header_menu').append(header_menu_elements + header_menu_dropdown);
 
-         $('.navleft_wrapper').empty();
+         } else {
 
-         if ($('body').width() + 17 < 992){
-
-             $('.navleft_wrapper').append('<li class="nav_item"><p class="nav_text">Основные ссылки</p></li>' + header_menu_elements + header_menu_dropdown_elements + '<li role="separator" class="divider"></li>' + leftnav_elements_curent);
-
-         } else{
-
-             $('.navleft_wrapper').append(leftnav_elements_curent);
+             $('.header_menu').append('<p class="header_text header_text-title">Навигация</p><div class="divider"></div>' + header_menu_elements + header_menu_dropdown_elements);
 
          }
 
      }
 
 
+
+     /*
+      *  Create Header Menu Elements
+      *
+      *  @var header_menu_nav_items  -  array of header_menu elements
+      *  @var temp
+     */
+     function create_header_menu_elements() {
+
+      var
+         temp_header_menu_nav_items = [],
+         temp;
+
+
+      /*
+       *  Create header_menu_nav_items array
+      */
+
+      $('.header_menu .header_button').each(function(){
+
+          temp = {
+              html: $(this).html(),
+              href: $(this)[0]['href'],
+              class: $(this)[0]['className'],
+              width: $(this).width() + 36
+          }
+
+          temp_header_menu_nav_items.push(temp);
+
+      });
+
+      return temp_header_menu_nav_items;
+
+    }
+
 });
+
 
 
 
