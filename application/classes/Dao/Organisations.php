2191fbea292bac55af6f2b35b50cfd071c03a64a
George Berezhnoy <gohabereg@gmail.com>
2017-03-10 11:59:03 +0300

    New organization model
    And some user model improvements

---
+++ application/classes/Dao/Organisations.php
@@ -0,0 +1,9 @@
+<?php defined('SYSPATH') or die('No direct access to script');
+
+class Dao_Organizations extends Dao_MySQL_Base {
+
+    protected $cache_key = 'Dao_Organizations';
+    protected $table = 'Organizations';
+
+}
+
