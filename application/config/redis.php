feb47764f633a32eab93e911215ec5e6e72a5294
Murod Khaydarov <murod.haydarov@gmail.com>
2017-03-21 16:56:06 +0300

    merging conflicts

---
+++ application/config/redis.php
@@ -0,0 +1,11 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+
+return array
+(
+	'default' => array
+	(
+		'hostname' => 'redis',
+		'port' => '6379',
+		'password' => '21gJs32hv3ks',
+	),
+);
