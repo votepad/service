237d28484c3a38b14926afe68419fbaa754b99ae
George Berezhnoy <gohabereg@gmail.com>
2017-04-04 20:20:46 +0300

    Fix

--- application/classes/Model/Organization.php
+++ application/classes/Model/Organization.php
@@ -111,15 +111,17 @@ class Model_Organization extends Model
             if (property_exists($this, $fieldname)) $insert->set($fieldname, $value);
         }
 
-        $id = $insert->execute();
+        $result = $insert->execute();
+
+        $this->fill_by_row($result);
 
         Dao_UsersOrganizations::insert()
             ->set('u_id', $this->owner)
-            ->set('o_id', $id)
+            ->set('o_id', $this->id)
             ->clearcache('User:' . $this->owner)
             ->execute();
 
-        return $this->get_($id);
+        return $this;
 
     }
 
@@ -131,20 +133,21 @@ class Model_Organization extends Model
     public function update()
     {
 
-        $insert = Dao_Organizations::update();
+        $insert = Dao_Users::update();
 
         foreach ($this as $fieldname => $value) {
             if (property_exists($this, $fieldname)) $insert->set($fieldname, $value);
         }
 
-        $insert->where('id', '=', $this->id);
         $insert->clearcache($this->id);
+        $insert->where('id', '=', $this->id);
 
-        $id = $insert->execute();
+        $insert->execute();
 
-        return $this->get_($id);
+        return $this->get_($this->id);
 
     }
+
 
     /**
      * @public
