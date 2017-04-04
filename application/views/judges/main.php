62d11803e592865fc94c454bea5e8ad11278558b
Toshiksvg <toxa.dj@mail.ru>
2017-04-04 20:51:17 +0300

    Add choto-tam

--- application/views/judges/main.php
+++ application/views/judges/main.php
@@ -8,16 +8,18 @@
 
         <!-- =============== VENDOR STYLES ===============-->
         <link rel="stylesheet" href="<?=$assets; ?>vendor/fontawesome/css/font-awesome.min.css">
-        <link rel="stylesheet" href="<?=$assets; ?>css/icons_fonts.css">
-        <link rel="stylesheet" href="<?=$assets; ?>css/app_v1.css">
+        <link rel="stylesheet" href="<?=$assets; ?>static/css/icons_fonts.css">
+        <link rel="stylesheet" href="<?=$assets; ?>static/css/app_v1.css">
+        <link rel="stylesheet" href="<?=$assets; ?>frontend/modules/css/header.css">
         <link rel="stylesheet" href="<?=$assets; ?>css/judges.css">
 
 
         <!-- =============== VENDOR SCRIPTS ===============-->
         <script type="text/javascript" src="<?=$assets; ?>vendor/jquery/dist/jquery.js"></script>
         <script type="text/javascript" src="<?=$assets; ?>vendor/bootstrap-notify/bootstrap-notify.js"></script>
-        <script type="text/javascript" src="<?=$assets; ?>js/app_v1.js"></script>
-        <script type="text/javascript" src="<?=$assets; ?>js/judges/judges.js"></script>
+        <script type="text/javascript" src="<?=$assets; ?>static/js/app_v1.js"></script>
+        <script type="text/javascript" src="<?=$assets; ?>static/js/judges/judges.js"></script>
+        <script type="text/javascript" src="<?=$assets; ?>frontend/bundles/votepad.bundle.js"></script>
     </head>
     <body>
 
@@ -26,18 +28,73 @@
 
 
     <div id="stage_nav" class="stage_nav">
-        <ul class="stage_nav-ul">
-            <li class="stage_nav_ul-li">Деловая игра</li>
-            <li class="stage_nav_ul-li">Доклад участника</li>
+        <ul class="stage_nav-ul tabs__header">
+            <li class="stage_nav_ul-li tabs__btn tabs__btn--active" data-toogle="tabs" data-block="qw">Деловая игра</li>
+            <li class="stage_nav_ul-li tabs__btn " data-toogle="tabs" data-block="qw1">Доклад участника</li>
             <li class="stage_nav_ul-li">Доклад участника</li>
             <li class="stage_nav_ul-li">Творческий этап</li>
             <li class="stage_nav_ul-li">Творческий этап</li>
         </ul>
     </div>
 
-        <div class="stages_holder"> <!--Держатель этапов-->
+        <div class="stages_holder tabs__content"> <!--Держатель этапов-->
 
-            <div class="stages_holder-stage"> <!--Один этап-->
+            <div class="stages_holder-stage tabs__block tabs__block--active" id="qw"> <!--Один этап-->
+
+                <div class="stages_holder_stage-peoples"> <!--Человек на этапе-->
+
+                    <div id="LOL" class="stages_holder_stage_peoples-criteria-holder"> <!--Критерии для человека-->
+
+                        <div class="stages_holder_stage_peoples_criteria_holder-criteria"> <!--Критерий-->
+                            <h1>КЕЕЕК</h1>
+                        </div>
+
+                        <div class="stages_holder_stage_peoples_criteria_holder-criteria"> <!--Критерий-->
+                            <h1>ЛОООЛ</h1>
+                        </div>
+
+                        <div class="stages_holder_stage_peoples_criteria_holder-criteria"> <!--Критерий-->
+                            <h1>ХЕХ</h1>
+                        </div>
+
+                        <div class="stages_holder_stage_peoples_criteria_holder-criteria"> <!--Критерий-->
+                            <h1>КЕЕЕК</h1>
+                        </div>
+
+                        <div class="stages_holder_stage_peoples_criteria_holder-criteria"> <!--Критерий-->
+                            <h1>ЛОООЛ</h1>
+                        </div>
+
+                        <div class="stages_holder_stage_peoples_criteria_holder-criteria"> <!--Критерий-->
+                            <h1>ХЕХ</h1>
+                        </div>
+
+                    </div>
+
+
+                </div>
+
+                <div class="stages_holder_stage-peoples"> <!--Человек на этапе-->
+
+                    <div class="stages_holder_stage_peoples-criteria-holder"> <!--Критерии для человека-->
+
+                        <div class="stages_holder_stage_peoples_criteria_holder-criteria"> <!--Критерий-->
+                            <h1>ХЕХ</h1>
+                        </div>
+
+                        <div class="stages_holder_stage_peoples_criteria_holder-criteria"> <!--Критерий-->
+                            <h1>ХАХ</h1>
+                        </div>
+
+                    </div>
+
+
+                </div>
+
+
+            </div>
+
+            <div class="stages_holder-stage tabs__block" id="qw1"> <!--Один этап-->
 
                 <div class="stages_holder_stage-peoples"> <!--Человек на этапе-->
 
@@ -118,4 +175,7 @@
            </div>
        </div>-->
     </body>
+    <script type="text/javascript">
+        vp.tabs.init();
+    </script>
 </html>
