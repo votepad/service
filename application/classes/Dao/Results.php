2cfed976c11b662ecb0ecc6c305f98456679b668
George Berezhnoy <gohabereg@gmail.com>
2017-04-08 20:47:20 +0300

    Results

---
+++ application/classes/Dao/Results.php
@@ -0,0 +1,8 @@
+<?php defined('SYSPATH') or die('No direct access to script');
+
+class Dao_Results extends Dao_MySQL_Base {
+
+    protected $cache_key = 'Dao_Results';
+    protected $table = 'Results';
+
+}
---
+++ application/classes/Model/Result.php
@@ -0,0 +1,92 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+/**
+ * Class Model_Result
+ * CRUD
+ */
+class Model_Result extends Model {
+
+    public $id;
+    public $event;
+    public $name;
+    public $description;
+    public $formula;
+
+    public function __construct($id = null) {
+
+        if ( !empty($id) ) {
+            $this->get_($id);
+        }
+
+    }
+
+    private function fill_by_row($db_selection) {
+
+        if (empty($db_selection['id'])) return $this;
+
+        foreach ($db_selection as $fieldname => $value) {
+            if (property_exists($this, $fieldname)) $this->$fieldname = $value;
+        }
+
+        return $this;
+
+    }
+
+    private function get_($id) {
+
+        $select = Dao_Results::select()
+            ->where('id', '=', $id)
+            ->limit(1)
+            ->cached(Date::MINUTE * 5, $id)
+            ->execute();
+
+        $this->fill_by_row($select);
+
+        return $this;
+
+    }
+
+    /**
+     * Saves Result to Database
+     */
+    public function save()
+    {
+
+        $this->dt_create = Date::formatted_time('now', 'Y-m-d');
+
+        $insert = Dao_Results::insert();
+
+        foreach ($this as $fieldname => $value) {
+            if (property_exists($this, $fieldname)) $insert->set($fieldname, $value);
+        }
+
+        $result = $insert->execute();
+
+        return $this->fill_by_row($result);
+
+    }
+
+    /**
+     * Updates Result data in database
+     *
+     * @return Model_Result
+     */
+    public function update()
+    {
+
+        $insert = Dao_Results::update();
+
+        foreach ($this as $fieldname => $value) {
+            if (property_exists($this, $fieldname)) $insert->set($fieldname, $value);
+        }
+
+        $insert->clearcache($this->id);
+        $insert->where('id', '=', $this->id);
+
+        $insert->execute();
+
+        return $this->get_($this->id);
+
+    }
+
+}
