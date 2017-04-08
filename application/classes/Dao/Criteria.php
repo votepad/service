96e06b6e24acf4de03c5b45c84c9ea27e302c9af
George Berezhnoy <gohabereg@gmail.com>
2017-04-08 20:46:07 +0300

    Models

---
+++ application/classes/Dao/Criteria.php
@@ -0,0 +1,9 @@
+<?php defined('SYSPATH') or die('No direct access to script');
+
+class Dao_Criteria extends Dao_MySQL_Base {
+
+    protected $cache_key = 'Dao_Criteria';
+    protected $table = 'Criteria';
+
+}
+
