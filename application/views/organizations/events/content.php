97ce809547b21f4ae36d46ad6d8d3263e8f272a3
Nikolay <turov96@yandex.ru>
2017-04-13 01:17:37 +0300

    organization page add layout

--- application/views/organizations/events/content.php
+++ application/views/organizations/events/content.php
@@ -1,14 +1,22 @@
-<!-- =============== PAGE STYLE ===============-->
-<link rel="stylesheet" href="<?=$assets; ?>static/css/org.css?v=<?= filemtime("assets/static/css/org.css") ?>">
+<div class="section__wrapper">
 
-<div class="jumbotron block">
+    <!-- =============== PAGE STYLE ===============-->
+    <link rel="stylesheet" href="<?=$assets; ?>static/css/org.css?v=<?= filemtime("assets/static/css/org.css") ?>">
 
-    <!-- Jumbotron Wrapper -->
-    <?=View::factory('organizations/blocks/jumbotron_wrapper', array('organization' => $organization)); ?>
+    <div class="jumbotron block">
 
-    <!-- Jumbotron Navigation -->
-    <div class="jumbotron_nav">
-        <?=$jumbotron_navigation; ?>
+        <!-- Jumbotron Wrapper -->
+        <?=View::factory('organizations/blocks/jumbotron_wrapper', array('organization' => $organization)); ?>
+
+        <!-- Jumbotron Navigation -->
+        <div class="jumbotron_nav">
+            <?=$jumbotron_navigation; ?>
+        </div>
+
     </div>
+
+    <section class="section__content">
+
+    </section>
 
 </div>
