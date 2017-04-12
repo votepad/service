2b50157424abae93678e2aa215f5aa863242ed81
Nikolay <turov96@yandex.ru>
2017-04-12 16:41:47 +0300

    improvements

--- assets/frontend/modules/js/formula.js
+++ assets/frontend/modules/js/formula.js
@@ -1,28 +1,89 @@
+/**
+ * Module for printing || creating || editing formulas
+ *
+ * @usage: formula.create(element, options);
+ *
+ * @element => HTML element
+ * @options => { 'mode': @mode, 'allItems': @allItems, 'curItems': @curItems }
+ *
+ * @mode     => print||create||edit
+ * @curItems => [{'id':5, 'name':'Name1", 'coeff':'0.5'},{'id':6, 'name':'Name2", 'coeff':'0.4'}]
+ * @allItems => [{'id':5, 'name':'Name1"},{'id':6, 'name':'Name2"}]
+ *
+ * @mode is required
+ * Allow to use @allItems or @curItems or both
+ */
+
 var formula = function(formula) {
 
 
-    var __formulaList   = null,
-        __additionList  = [];
+    var modes = {
+            PRINT: "print",
+            CREATE: "create",
+        },
+        input = null,
+        formulaNodes  = {
+            list: null,
+            additionList: null,
+            additionItems: []
+        };
 
-    
+
+
+    /**
+     * Create DOM element
+     * @param el - string => 'div' || 'ul'
+     * @param className - string => 'class1 class2'
+     * @param html - string => 'text'
+     * @returns {Element}
+     */
+    function createElement(el,className,html) {
+        var el = document.createElement(el);
+
+        className.split(' ').forEach(function (name) {
+            el.classList.add(name);
+        });
+
+        el.innerHTML = html ? html : '';
+
+        return el;
+    }
+
+
+    /**
+     * New formula
+     * @param el - HTMLElement
+     * @param options - Object
+     */
     function formula(el, options) {
 
-        if (!(el && el.nodeType && el.nodeType === 1)) {
+        if (!el.nodeType === 1) {
             throw 'formula: `el` must be HTMLElement, and not ' + {}.toString.call(el);
         }
 
 
-        this.el = el;
-        this.mode = options.mode;
-        this.allItems = __parseItems(options.allItems);
-        this.curItems = __parseItems(options.curItems);
-        this.toJSON   = __toJSON;
+        this.el       = el;
+        this.mode     = options.mode;
+        this.allItems = parseItems(options.allItems);
+        this.curItems = parseItems(options.curItems);
+        this.toJSON   = toJSON;
 
-        if (this.mode === "print") {
-            __print(this);
-        } else if (this.mode === "create") {
-            __create(this);
+        switch(this.mode) {
+
+            case modes.PRINT:
+                print(this);
+                break;
+
+            case modes.CREATE:
+                create(this);
+                break;
+
+            default:
+                console.log("Mode is not allowed");
+                break;
+
         }
+
     }
 
 
@@ -31,177 +92,189 @@ var formula = function(formula) {
      * @param formula
      * @private
      */
-    function __create(formula) {
+    function create(formula) {
 
-        var area    = document.createElement('div'),
-            label   = document.createElement('label'),
-            ul      = document.createElement('ul'),
-            input   = document.createElement('input');
+        var area    = createElement('div','formula__area clear_fix'),
+            label   = createElement('label','formula__label', 'Формула'),
+            addBtn  = createElement('div','formula__item-add'),
+            addIcon = createElement('i', 'fa fa-plus');
 
-        area.className  = "formula__area clear_fix";
-        label.className = "formula__label";
-        ul.className    = "formula__list";
 
-        var add = document.createElement('div'),
-            icon_add = document.createElement('i');
+        formulaNodes.list          = createElement('ul','formula__list');
+        formulaNodes.additionList  = createElement('ul', 'formula__addition-list hide');
+        formulaNodes.additionItems = createAdditionItems(formula.allItems);
 
-        add.className = "formula__item-add";
-        icon_add.className = "fa fa-plus";
-        label.innerHTML = "Формула";
+        input      = document.createElement('input');
         input.type = "hidden";
         input.name = "formula";
 
-        add.append(icon_add);
-        area.append(input);
-        area.append(label);
-        area.append(ul);
-        area.append(add);
+        area.appendChild(input);
+        area.appendChild(label);
+        area.appendChild(formulaNodes.list);
+        addBtn.appendChild(addIcon);
+        addBtn.appendChild(formulaNodes.additionList);
+        area.appendChild(addBtn);
 
         formula.el.appendChild(area);
 
-        __formulaList = ul;
 
-        __createAdditionList(formula.allItems);
-        document.body.addEventListener('click', __hideAdditionList);
-        add.addEventListener('click', __showAdditionList);
+        /**
+         * TODO: change event listener
+         */
+
+        document.body.addEventListener('click', hideAdditionList);
+        addBtn.addEventListener('click', showAdditionList);
 
     }
 
 
-    /**
-     * Add item to `formula__list`
-     * Remove item from __additionList
-     * @param event
-     * @private
-     */
-    function __addFormulaItem(event) {
-        var element = __additionList[event.target.id];
-        __additionList.splice(event.target.id, 1);
 
-        var item = __createFormulaItem(element);
-        __formulaList.append(item);
-    }
+
 
 
     /**
-     * Create item for `formula__list`
+     * Create item for `formulaNodes.list`
      * @param element
+     * @returns {Element}
      * @private
      */
-    function __createFormulaItem(element) {
-        var item = document.createElement('div'),
-            close = document.createElement('i'),
-            input = document.createElement('input'),
-            multiply = document.createElement('i'),
-            name = document.createElement('span');
+    function createFormulaItem(element) {
+        var item     = createElement('div', 'formula__item'),
+            close    = createElement('i', 'fa fa-times formula__item-icon'),
+            input    = createElement('input', 'formula__item-input'),
+            multiply = createElement('i', 'fa fa-circle formula__item-multiply'),
+            name     = createElement('span', 'formula__item-name', element.innerHTML);
 
-        item.className = "formula__item";
         item.dataset.id = element.dataset.id;
-        close.className = "fa fa-times formula__item-icon";
+
         close.dataset.id = element.id;
         close.dataset.name = element.innerHTML;
-        close.addEventListener('click', __removeFormulaItem);
-        input.className = "formula__item-input";
+        close.addEventListener('click', removeFormulaItem);
+
         input.value = "1";
         input.type = "number";
         input.step = "0.1";
-        multiply.className = "fa fa-circle formula__item-multiply";
-        name.className = "formula__item-name";
-        name.innerHTML = element.innerHTML;
 
-        item.append(close);
-        item.append(input);
-        item.append(multiply);
-        item.append(name);
+        item.appendChild(close);
+        item.appendChild(input);
+        item.appendChild(multiply);
+        item.appendChild(name);
 
         return item;
     }
 
 
     /**
-     * Remove item from `formula__list`
-     * Add item to __additionList
-     * @param event
+     * Add item to `formulaNodes.list`
+     * Remove item from `formulaNodes.additionItems`
      * @private
      */
-    function __removeFormulaItem(event) {
-        var li = document.createElement('li');
-        li.className = "formula__addition-item";
+    function addFormulaItem(event) {
+        var element = formulaNodes.additionItems[event.target.id];
+        formulaNodes.additionItems.splice(event.target.id, 1);
+
+        var item = createFormulaItem(element);
+        formulaNodes.list.appendChild(item);
+    }
+
+
+    /**
+     * Remove item from `formulaNodes.list`
+     * Add item to `formulaNodes.additionItems`
+     * @private
+     */
+    function removeFormulaItem(event) {
+        var li = createElement('li', 'formula__addition-item', event.target.dataset.name);
+
         li.dataset.id = event.target.dataset.id;
-        li.innerHTML = event.target.dataset.name;
-        li.addEventListener('click', __addFormulaItem);
-        __additionList.push(li);
+        li.addEventListener('click', addFormulaItem);
+
+        formulaNodes.additionItems.push(li);
         event.target.parentNode.remove();
     }
 
 
-    /**
-     * Show AdditionList on click
-     * @param event
-     * @private
-     */
-    function __showAdditionList(event) {
-        if (event.target.parentNode.children.length == 1) {
-            event.target.parentNode.append(__printAdditionList());
-        }
-    }
-
 
     /**
-     * Hide AdditionList on click
-     * @param event
+     * Show `formulaNodes.additionItems` on click
      * @private
      */
-    function __hideAdditionList(event) {
-        var list = document.querySelector('.formula__addition-list');
-        if (!event.target.parentNode.classList.contains('formula__item-add') && list !== null) {
-            list.remove();
-        }
-    }
+    function showAdditionList() {
 
+        getAdditionItems();
 
-    /**
-     * Print AdditionList
-     * @returns {Element}
-     * @private
-     */
-    function __printAdditionList() {
-        var ul = document.createElement('ul');
-            ul.className = "formula__addition-list";
-
-        if (__additionList.length > 0) {
-            for (var i = 0; i < __additionList.length; i++) {
-                __additionList[i].id = i;
-                ul.append(__additionList[i]);
-            }
+        if ( formulaNodes.additionList.classList.contains('hide') ) {
+            formulaNodes.additionList.classList.remove('hide');
         } else {
-            var span = document.createElement('span');
-            span.className = "formula__addition-item";
-            span.innerHTML = "Элементы не найдены";
-            ul.append(span);
+            formulaNodes.additionList.classList.add('hide');
         }
 
-        return ul;
     }
 
 
     /**
-     * Create Addition List on init
-     * @param items
+     * Hide `formulaNodes.additionItems` on click
      * @private
      */
-    function __createAdditionList(items) {
+    function hideAdditionList(event) {
 
-        for (var i = 0; i < items.length; i++) {
-            var li = document.createElement('li');
-            li.className = "formula__addition-item";
-            li.dataset.id = items[i].id;
-            li.innerHTML = items[i].name;
-            li.addEventListener('click', __addFormulaItem);
-            __additionList.push(li);
+        if ( ! formulaNodes.additionList.classList.contains('hide') &&
+             ! event.target.parentNode.classList.contains('formula__item-add') )
+        {
+
+            formulaNodes.additionList.classList.add('hide');
+
         }
 
     }
+
+
+    /**
+     * Create `formulaNodes.additionItems`
+     * @param items
+     * @returns {Array}
+     * @private
+     */
+    function createAdditionItems(items) {
+        var arr = [], i, li;
+
+        for (i = 0; i < items.length; i++) {
+
+            li = createElement('li', 'formula__addition-item', items[i].name);
+            li.dataset.id = items[i].id;
+
+            li.addEventListener('click', addFormulaItem);
+            arr.push(li);
+        }
+
+        return arr;
+    }
+
+
+    /**
+     * Get `formulaNodes.additionItems`
+     * @private
+     */
+
+    function getAdditionItems() {
+
+        formulaNodes.additionList.innerHTML = "";
+
+        if (formulaNodes.additionItems.length > 0) {
+
+            for (var i = 0; i < formulaNodes.additionItems.length; i++) {
+                formulaNodes.additionItems[i].id = i;
+                formulaNodes.additionList.appendChild(formulaNodes.additionItems[i]);
+            }
+
+        } else {
+
+            formulaNodes.additionList.appendChild( createElement('span', 'formula__addition-item', 'Элементы не найдены') );
+
+        }
+
+    }
+
 
 
     /**
@@ -209,39 +282,24 @@ var formula = function(formula) {
      * @param formula
      * @private
      */
-    function __print(formula) {
+    function print(formula) {
 
-        var item, name, coeff, plus, multiply;
+        var item, name, coeff, multiply, i;
 
-        for (var i = 0; i < formula.curItems.length; i++) {
-            item = document.createElement('span');
-            item.classList = "formula__print";
+        for (i = 0; i < formula.curItems.length; i++) {
+
+            item = createElement('span', 'formula__print');
             item.dataset.id = formula.curItems[i].id;
 
-            plus = document.createElement('span');
-            plus.className = "formula__print-math";
-            plus.innerHTML = '<i class="fa fa-plus" aria-hidden="true"></i>';
+            multiply = createElement('span', 'formula__print-math', '<i class="fa fa-times" aria-hidden="true"></i>');
+            name     = createElement('span', 'formula__print-name', formula.curItems[i].name);
+            coeff    = createElement('span', 'formula__print-coeff', formula.curItems[i].coeff);
 
-            multiply = document.createElement('span');
-            multiply.className = "formula__print-math";
-            multiply.innerHTML = '<i class="fa fa-times" aria-hidden="true"></i>';
+            item.appendChild(name);
+            item.appendChild(multiply);
+            item.appendChild(coeff);
 
-            name = document.createElement('span');
-            name.className = "formula__print-name";
-            name.innerHTML = formula.curItems[i].name;
-
-            coeff = document.createElement('span');
-            coeff.className = "formula__print-coeff";
-            coeff.innerHTML = formula.curItems[i].coeff;
-
-            item.append(name);
-            item.append(multiply);
-            item.append(coeff);
-            if (i + 1 != formula.curItems.length) {
-                item.append(plus);
-            }
-
-            formula.el.append(item)
+            formula.el.appendChild(item);
         }
 
     }
@@ -253,29 +311,29 @@ var formula = function(formula) {
      * @returns null || JSON Object
      * @private
      */
-    function __parseItems(items) {
-        if (items === undefined) {
-            return null;
-        } else {
+    function parseItems(items) {
+        try {
             return JSON.parse(items);
+        } catch (e) {
+            return null;
         }
     }
 
 
-    function __toJSON() {
-        var arr = [], out;
+    function toJSON() {
+        var arr = [], out, i;
 
-        for (var i = 1; i < __formulaList.children.length; i++) {
+        for (i = 0; i < formulaNodes.list.childElementCount; i++) {
             out = {
-                "id": __formulaList.children[i].dataset.id,
-                "name":__formulaList.children[i].children[3].innerHTML,
-                "coeff":__formulaList.children[i].children[1].value
+                "id": formulaNodes.list.children[i].dataset.id,
+                "name":formulaNodes.list.children[i].children[3].innerHTML,
+                "coeff":formulaNodes.list.children[i].children[1].value
 
             };
             arr.push(out);
         }
-        return JSON.stringify(arr);
 
+        input.value = JSON.stringify(arr);
     }
 
 
@@ -291,7 +349,7 @@ var formula = function(formula) {
 
 
     // Export
-    formula.version = '0.0.0';
+    formula.version = '0.0.1';
     return formula;
 
 }();
