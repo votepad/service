1e8792878777667c7acc6f3ae63677871241f2b8
Toshiksvg <toxa.dj@mail.ru>
2017-04-15 16:38:10 +0300

    Удаление лишних тегов

--- application/views/judgepanel/panels/panel1.php
+++ application/views/judgepanel/panels/panel1.php
@@ -3,15 +3,6 @@
 
 
 <section class="section__content">
-
-    <!--<div id="stage_nav" class="stage_nav stage_nav-ul tabs__header">
-            <div class="stage_nav_ul-li tabs__btn tabs__btn--active" data-toggle="tabs" data-block="qw">Деловая игра</div>
-            <div class="stage_nav_ul-li tabs__btn " data-toggle="tabs" data-block="qw1">Доклад участника</div>
-            <div class="stage_nav_ul-li">Доклад участника</div>
-            <div class="stage_nav_ul-li">Творческий этап</div>
-            <div class="stage_nav_ul-li">Творческий этап</div>
-    </div>-->
-
     <div name="stage_nav"></div>
 
     <div class="stages_holder tabs__content"> <!--Держатель этапов-->
@@ -66,12 +57,6 @@
                                 <span name="vp-custom-radiobox" data-name="vp-radiobox-1" data-value="4">4</span>
                                 <span name="vp-custom-radiobox" data-name="vp-radiobox-1" data-value="5">5</span>
                                 <span name="vp-custom-radiobox" data-name="vp-radiobox-1" data-value="6">6</span>
-                                <span name="vp-custom-radiobox" data-name="vp-radiobox-2" data-value="1">1</span>
-                                <span name="vp-custom-radiobox" data-name="vp-radiobox-2" data-value="2">2</span>
-                                <span name="vp-custom-radiobox" data-name="vp-radiobox-2" data-value="3">3</span>
-                                <span name="vp-custom-radiobox" data-name="vp-radiobox-2" data-value="4">4</span>
-                                <span name="vp-custom-radiobox" data-name="vp-radiobox-2" data-value="5">5</span>
-                                <span name="vp-custom-radiobox" data-name="vp-radiobox-2" data-value="6">6</span>
                              </div>
 
 
@@ -79,71 +64,7 @@
 
                     </div>
 
-                    <div class="stages_holder_stage_peoples_criteria_holder-criteria"> <!--Критерий-->
-                        <h1>КЕЕЕК</h1>
-                    </div>
-
-                    <div class="stages_holder_stage_peoples_criteria_holder-criteria"> <!--Критерий-->
-                        <h1>ЛОООЛ</h1>
-                    </div>
-
-                    <div class="stages_holder_stage_peoples_criteria_holder-criteria"> <!--Критерий-->
-                        <h1>ХЕХ</h1>
-                    </div>
-
                 </div>
-
-            </div>
-
-            <div class="stages_holder_stage-peoples"> <!--Человек на этапе-->
-
-                <div class="stages_holder_stage_peoples-criteria-holder"> <!--Критерии для человека-->
-
-                    <div class="stages_holder_stage_peoples_criteria_holder-criteria"> <!--Критерий-->
-                        <h1>ХАХ</h1>
-                    </div>
-
-                    <div class="stages_holder_stage_peoples_criteria_holder-criteria"> <!--Критерий-->
-                        <div class="content-block-crit">
-                            <div class="m-l-10">
-                                <div class="titleS m-t-20 fl_l text-left">МДаааааа Антон</div>
-                                <i class="m-t-10 m-b-10 m-l-10 m-r-10 fl_r fa fa-hand-o-right" aria-hidden="true"></i>
-                            </div>
-
-
-                            <div class="Balls  m-t-10 m-b-5 m-l-10 fl_l text-left">
-                                Итоговый балл: из 15
-                                <i class="fa fa-graduation-cap" aria-hidden="true"></i>
-                            </div>
-
-
-
-                            <div class="fl_l m-t-10 m-l-10">
-                                <span name="vp-custom-radiobox" data-name="scores_part_id" data-value="">1</span>
-                                <span name="vp-custom-radiobox" data-name="scores_part_id">2</span>
-                                <span name="vp-custom-radiobox" data-name="scores_part_id">3</span>
-                                <span name="vp-custom-radiobox" data-name="scores_part_id">4</span>
-                                <span name="vp-custom-radiobox" data-name="5">5</span>
-                                <span name="vp-custom-radiobox" data-name="6">6</span>
-                                <span name="vp-custom-radiobox" data-name="1">1</span>
-                                <span name="vp-custom-radiobox" data-name="2">2</span>
-                                <span name="vp-custom-radiobox" data-name="3">3</span>
-                                <span name="vp-custom-radiobox" data-name="4">4</span>
-                                <span name="vp-custom-radiobox" data-name="5">5</span>
-                                <span name="vp-custom-radiobox" data-name="6">6</span>
-                            </div>
-
-
-                        </div>
-
-                    </div>
-
-
-
-
-
-                </div>
-
 
             </div>
 
@@ -156,27 +77,57 @@
                 <div id="LOL" class="stages_holder_stage_peoples-criteria-holder"> <!--Критерии для человека-->
 
                     <div class="stages_holder_stage_peoples_criteria_holder-criteria"> <!--Критерий-->
+
                         <div class="pictures">
                             <img class="lol" src="/assets/static/img/lol.jpg">
                         </div>
 
                         <div class="content-block">
-                            <div class="titleS text-left">МДаааааа Антон</div>
-                            <div class="Balls text-left"> Итоговый балл: <span class="BallsBlue">13</span> из 15</div>
+                            <div class="titleS text-left">Мдаааааааа Антон</div>
+                            <div class="Balls m-t-10 m-b-5 text-left"> Итоговый балл: <span class="BallsBlue">13</span> из 15</div>
                             <div class="SlideElem">
-                                <div class="Smah ">Для оценивания смахните вправо
-                                    <i class="m-t-10 m-b-10 m-l-0 m-r-0 fl_l fa fa-hand-o-right" aria-hidden="true"></i>
-                                </div>
+                                <div class="Smah text-left">Для оценивания смахните вправо</div>
+                                <i class="LolIcon fa fa-hand-o-right" aria-hidden="true"></i>
                             </div>
 
 
                         </div>
+
+
+
                     </div>
 
+                    <div class="stages_holder_stage_peoples_criteria_holder-criteria"> <!--Критерий-->
+                        <div class="content-block-crit">
 
+                            <div class="titleS m-l-10 m-t-10 text-left">МДаааааа Антон
+                                <i class="fl_r fa fa-hand-o-right" aria-hidden="true"></i>
+                            </div>
+
+
+
+                            <div class="Balls  m-l-10 m-t-10 m-b-5 m-l-10 text-left">
+                                Итоговый балл: из 15
+                                <i class="fa fa-graduation-cap" aria-hidden="true"></i>
+                            </div>
+
+
+
+                            <div class="fl_l m-t-10 m-l-10">
+                                <span name="vp-custom-radiobox" data-name="vp-radiobox-2" data-value="1">1</span>
+                                <span name="vp-custom-radiobox" data-name="vp-radiobox-2" data-value="2">2</span>
+                                <span name="vp-custom-radiobox" data-name="vp-radiobox-2" data-value="3">3</span>
+                                <span name="vp-custom-radiobox" data-name="vp-radiobox-2" data-value="4">4</span>
+                                <span name="vp-custom-radiobox" data-name="vp-radiobox-2" data-value="5">5</span>
+                                <span name="vp-custom-radiobox" data-name="vp-radiobox-2" data-value="6">6</span>
+                            </div>
+
+
+                        </div>
+
+                    </div>
 
                 </div>
-
 
             </div>
 
@@ -184,27 +135,4 @@
 
 
     </div>
-
-
-
-
-    <!--<div class="row">
-        <div class="col-xs-12">
-
-            <card class="card card_withimage clear_fix open" id="team_17">
-
-                <div class="card_image" id="logo_team_17">
-                    <img src="/uploads/teams/m_" alt="">
-                </div>
-
-                <div class="card_withimage card_title">
-                    <div class="card_title-text" id="name_team_17">
-                        ваолмвталом
-                    </div>
-                </div>
-
-            </card>
-
-        </div>
-    </div>-->
 </section>
