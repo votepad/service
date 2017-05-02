72da9eb96116027f1a722664f762d573ff1d07ac
Nikolay <turov96@yandex.ru>
2017-05-02 19:28:45 +0300

    fixed

--- application/views/organizations/settings/jumbotron_navigation.php
+++ application/views/organizations/settings/jumbotron_navigation.php
@@ -1,14 +1,27 @@
+<?
+    $btns = array(
+        0 => array(
+            'uri' => URL::site('organization/' . $id . '/settings/main'),
+            'name' => "Об организации",
+            'flag' => URL::site('organization/' . $id . '/settings/main') == $_SERVER['REQUEST_URI'] ? 'active'  : ''
+        ),
+        1 => array(
+            'uri' => URL::site('organization/' . $id . '/settings/team'),
+            'name' => "Сотрудники",
+            'flag' => URL::site('organization/' . $id . '/settings/team') == $_SERVER['REQUEST_URI'] ? 'active'  : ''
+        ),
+    );
+?>
+
 
 <ul class="jumbotron__nav-list jumbotron__nav--org-list">
-    <li class="jumbotron__nav-item jumbotron__nav--org-item ">
-        <a class="jumbotron__nav-btn" href="<?=URL::site('organization/' . $id . '/settings/main'); ?>">
-            Об организации
-        </a>
-    </li>
-    <li class="jumbotron__nav-item jumbotron__nav--org-item ">
-        <a class="jumbotron__nav-btn" href="<?=URL::site('organization/' . $id . '/settings/team'); ?>">
-            Сотрудники
-        </a>
-    </li>
+
+    <? foreach ($btns as $btn): ?>
+        <li class="jumbotron__nav-item jumbotron__nav--org-item">
+            <a class="jumbotron__nav-btn <?= $btn['flag']; ?>" href="<?= $btn['uri']; ?>">
+                <?= $btn['name']; ?>
+            </a>
+        </li>
+    <?endforeach;?>
 
 </ul>
