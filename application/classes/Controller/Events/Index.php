8cb33ab24f4d8d4dd8961ca98d462748af244eca
Murod Khaydarov <murod.haydarov@gmail.com>
2017-05-16 18:01:33 +0300

    some improvements

--- application/classes/Controller/Events/Index.php
+++ application/classes/Controller/Events/Index.php
@@ -44,6 +44,22 @@ class Controller_Events_Index extends Dispatch
             $this->event = $event;
             $this->organization = new Model_Organization($event->organization);
 
+            $action = $this->request->action();
+
+            switch ($action) {
+                case 'result':
+                case 'landing':
+                    break;
+                default:
+
+                    /** do not allow */
+                    if (!self::isLogged() || !$this->event->isAssistant($this->user->id)) {
+                        $this->redirect('event/' . $this->event->id);
+                    }
+
+                    break;
+            }
+
             if (!$event->code || !Model_Event::getEventByCode($event->code)) {
                 $this->event->code = $event->generateCodeForJudges($event->id);
                 $this->event->update();
@@ -81,9 +97,6 @@ class Controller_Events_Index extends Dispatch
      */
     public function action_settings()
     {
-        if (!$this->event->isAssistant($this->user->id) || !self::isLogged()) {
-            $this->redirect('event/' . $this->event->id);
-        }
 
         $this->template->mainSection = View::factory('events/settings/content')
             ->set('event', $this->event)
@@ -193,10 +206,6 @@ class Controller_Events_Index extends Dispatch
      */
     public function action_stages()
     {
-        if (!$this->event->isAssistant($this->user->id) || !self::isLogged()) {
-            $this->redirect('event/' . $this->event->id);
-        }
-
         $stages = Methods_Stages::getByEvent($this->event->id);
         $members = array(
             'participants' => Methods_Participants::getByEvent($this->event->id),
